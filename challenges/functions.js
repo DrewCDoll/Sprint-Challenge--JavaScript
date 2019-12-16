8bbaf24f6a0e1d5b418d47c4ddbf8e61e02077e4
DrewCDoll <drwcdoll@gmail.com>
2019-12-15 21:24:57 -0500

    functions completed

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,25 +7,42 @@
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
 
+function consume(a, b, cb) {
+  return cb(a, b); 
+}
+
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+function add(a, b) {
+  return a + b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function greeting(a, b) {
+  return `Hello ${a} ${b}, nice to meet you!`;
+}
+
+
 
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 
 // Explain in your own words why nestedfunction can access the variable internal.
 
-// Explanation: 
+// Explanation: JS allows functions to look for variables declared in themselves and outside into their environment.  This is referred to as the lexical environment. Lexical environment is the same as scope. If nestedFunction had a variable (let or const) declared inside of itself myFunction could not access it. If the variable was declared with "var" myFunction could access it because "var" declarations are hoisted up to the global scope.
 
 
 const external = "I'm outside the function";
